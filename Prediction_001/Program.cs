97c5d05ec5a5501e3d92238d0a1cae5b8899f6cf
CARICOM12 <0190a@ukr.net>
2017-04-11 16:59:02 +0300

    It's a programm which predicts linguistic future

---
+++ Prediction_001/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prediction_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double zulu = 23.8;
+            double changeByDecadeZulu = 1.1;
+            for (int i = 0; zulu > 0; i++ )
+            {
+                zulu = zulu * 22.7;
+                Console.WriteLine("For {} decade value is {1} ", zulu, changeByDecadeZulu);
+            }
+            Console.ReadKey();
+        }
+    }
+}
