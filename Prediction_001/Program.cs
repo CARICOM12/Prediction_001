4f8c7308f25b14cbcaa94c98aa4809adaec339f3
CARICOM12 <0190a@ukr.net>
2017-04-14 00:15:06 +0300

    A programm was fixed and comments were added

--- Prediction_001/Program.cs
+++ Prediction_001/Program.cs
@@ -11,12 +11,18 @@ namespace Prediction_001
         static void Main(string[] args)
         {
             double zulu = 23.8;
-            double changeByDecadeZulu = 1.1;
-            for (int i = 0; zulu > 0; i++ )
+            double changeByDecadeZulu = 0.011;
+            for (int i = 0; zulu > 0.01; i++ )
             {
-                zulu = zulu * 22.7;
-                Console.WriteLine("For {} decade value is {1} ", zulu, changeByDecadeZulu);
+                Console.WriteLine("For the current {0} decade value is {1}, year {2} ", zulu, changeByDecadeZulu, i * 10);
+                zulu = zulu - zulu * changeByDecadeZulu;
+                
             }
+            //a0 = 100, t = 0.1
+            //a1 = a0 - (a0 * 0.1)//100-10 = 90
+            //a2 = a1 - (a1 * 0.1)//90 - 9 = 81
+            //a3 = a2 - (a2 * 0.1)//81 - 8.1 = 72.9
+
             Console.ReadKey();
         }
     }
